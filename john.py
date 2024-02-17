e842782a453f2ef3d86eacacf10de45f5ddcaaaa
jrbdino <117558328+jrbdino@users.noreply.github.com>
2024-02-17 14:00:34 -0600

    Added light blue.PNG, added locate light blue function

--- john.py
+++ john.py
@@ -4,7 +4,10 @@ import time
 time.sleep(1)
 screen_size = pyautogui.size()
 
-pyautogui.screenshot()
+pyautogui.screenshot(region=(5, 220, 1890, 830))
 
-print(pyautogui.locateOnScreen('Images/Target.png'))
-print("John Cyberclubb")
+for i in range(31):
+    x, y = pyautogui.locateCenterOnScreen('Images/light blue.PNG')
+    print(x, y)
+    pyautogui.click(x, y)
+    # print(pyautogui.mouseInfo())
