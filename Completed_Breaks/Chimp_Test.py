a61763eab2b4c388cf3e0425fb4026e10e797815
jrbdino <117558328+jrbdino@users.noreply.github.com>
2024-02-18 02:55:50 -0600

    fixed references in Chimp Test

--- Completed_Breaks/Chimp_Test.py
+++ Completed_Breaks/Chimp_Test.py
@@ -4,7 +4,7 @@ for n in range(21):
     seq_xy = []
     curr_len = 4 + n
     for i in range(1, curr_len + 1):
-        ref = '.\Images\Chimp_Test_Images\%s.png' %(str(i))
+        ref = '..\Images\Chimp_Test_Images\%s.png' %(str(i))
         print(ref)
         x, y = gui.locateCenterOnScreen(ref)
         seq_xy.append([x, y])
@@ -12,5 +12,5 @@ for n in range(21):
     for i in range(len(seq_xy)):
         gui.click(seq_xy[i][0], seq_xy[i][1])
 
-    x, y = gui.locateCenterOnScreen(".\Images\Chimp_Test_Images\Conf_Button.png")
+    x, y = gui.locateCenterOnScreen("..\Images\Chimp_Test_Images\Conf_Button.png")
     gui.click(x, y)
