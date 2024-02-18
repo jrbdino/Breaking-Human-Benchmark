10bc87360433cbaaa3bc8bff610975a4483a773b
Carssorole <86818471+Carssorole@users.noreply.github.com>
2024-02-18 02:11:13 -0600

    completed chimp test
    up to 24, which is good enough.  ong fr fr. Also fixed some old file references hat got changed.

--- Chimp_Test.py
+++ Chimp_Test.py
@@ -1,12 +1,16 @@
 import pyautogui as gui
 
-seq_xy = []
-curr_len = 4
-for i in range(1, curr_len + 1):
-    ref = 'Images\Chimp_Test_Images\%s.png' %(str(i))
-    print(ref)
-    x, y = gui.locateCenterOnScreen(ref)
-    seq_xy.append([x, y])
-print(seq_xy)
-for i in range(len(seq_xy)):
-    gui.click(seq_xy[i][0], seq_xy[i][1])
+for n in range(21):
+    seq_xy = []
+    curr_len = 4 + n
+    for i in range(1, curr_len + 1):
+        ref = 'Images\Chimp_Test_Images\%s.png' %(str(i))
+        print(ref)
+        x, y = gui.locateCenterOnScreen(ref)
+        seq_xy.append([x, y])
+    print(seq_xy)
+    for i in range(len(seq_xy)):
+        gui.click(seq_xy[i][0], seq_xy[i][1])
+
+    x, y = gui.locateCenterOnScreen("Images\Chimp_Test_Images\Conf_Button.png")
+    gui.click(x, y)
